704c52abdbaf661ca8df1bb708403dee501c5fbe
vladryanka <vsmorzhok@mail.ru>
2025-03-17 23:59:18 +0300

    Добавление собственного Action

--- app/src/main/java/com/smorzhok/broadcast/MainActivity.kt
+++ app/src/main/java/com/smorzhok/broadcast/MainActivity.kt
@@ -3,17 +3,35 @@ package com.smorzhok.broadcast
 import android.content.Intent
 import android.content.IntentFilter
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
 
 class MainActivity : AppCompatActivity() {
     private val receiver = MyReceiver()
+
+    private var count = 0
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        val filter = IntentFilter(Intent.ACTION_BATTERY_LOW)
-        registerReceiver(receiver,filter)
+        findViewById<Button>(R.id.button).setOnClickListener {
+            val intent = Intent(MyReceiver.ACTION_CLICKED)
+            intent.putExtra(MyReceiver.EXTRA_COUNT, ++count)
+            sendBroadcast(intent)
+
+        }
+        val filter =
+            IntentFilter().apply {
+                addAction(Intent.ACTION_BATTERY_LOW)
+                addAction(Intent.ACTION_AIRPLANE_MODE_CHANGED)
+                addAction(MyReceiver.ACTION_CLICKED)
+            }
+        ContextCompat.registerReceiver(
+            this,
+            receiver,
+            filter,
+            ContextCompat.RECEIVER_NOT_EXPORTED
+        )
     }
 
     override fun onDestroy() {
--- app/src/main/java/com/smorzhok/broadcast/MyReceiver.kt
+++ app/src/main/java/com/smorzhok/broadcast/MyReceiver.kt
@@ -5,12 +5,33 @@ import android.content.Context
 import android.content.Intent
 import android.widget.Toast
 
-class MyReceiver: BroadcastReceiver() {
+class MyReceiver : BroadcastReceiver() {
     override fun onReceive(context: Context?, intent: Intent?) {
-        when(intent?.action){
-            Intent.ACTION_BATTERY_LOW ->{
-                Toast.makeText(context, "Low battery", Toast.LENGTH_SHORT).show()
+        when (intent?.action) {
+            ACTION_CLICKED -> {
+                val count = intent.getIntExtra(EXTRA_COUNT, 0)
+                Toast.makeText(context, "Clicked $count", Toast.LENGTH_SHORT).show()
+            }
+
+            Intent.ACTION_BATTERY_LOW -> {
+                val turnedOn = intent.getIntExtra("state", 0)
+                Toast.makeText(context, "Low battery: $turnedOn", Toast.LENGTH_SHORT).show()
+            }
+
+            Intent.ACTION_AIRPLANE_MODE_CHANGED -> {
+                val turnedOn = intent.getBooleanExtra("state", false)
+                Toast.makeText(
+                    context,
+                    "Airplane mode changed. Turned on: $turnedOn",
+                    Toast.LENGTH_SHORT
+                ).show()
             }
         }
+    }
+
+    companion object{
+        const val ACTION_CLICKED = "clicked"
+
+        const val EXTRA_COUNT = "count"
     }
 }
